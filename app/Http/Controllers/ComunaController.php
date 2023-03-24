1c19cace2ba08f3fa6961b20293ab97cfee7724a
Esteban Vasquez <esteban98e31@gmail.com>
2023-03-23 21:05:03 -0500

    Ajuste de la vista del listado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Http\Controllers\Controller;
 use App\Models\Comuna;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ComunaController extends Controller
 {
@@ -16,8 +15,13 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
-        return view("comunas.index", ["comunas" => $comunas]);
+        //
+        //$comunas = Comuna::all();
+        $comunas=DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+            ->get();
+        return view("comuna.index", ['comunas' => $comunas]);
     }
 
     /**
--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -9,11 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
-    <title>Document</title>
+    <title>Listado de comunas</title>
 </head>
+
 <body>
     <div class="container">
         <h1>Comune list</h1>
@@ -31,15 +32,16 @@
     @foreach ($comunas as $comuna)
     <tr>
       <th scope="row">{{$comuna->comu_codi}}</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>{{$comuna->$comu_nomb}}</td>
+      <td>{{$comuna->muni_nomb}}</td>
+      <td><span> Acciones</span></td>
     </tr>
     @endforeach
   </tbody>
 </table>
     </div>   
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
